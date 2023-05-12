c74ca04090add284d4302e58b27d68729da243ca
nkp1111 <neerajp4321@gmail.com>
2023-05-12 11:09:26 +0530

    feat: edit profile info

--- routes/profile.js
+++ routes/profile.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 const { User } = require("../database/user")
+const { cloudinary } = require("../cloudinary")
 
 router.get("/", async (req, res) => {
   let user = null
@@ -11,6 +12,34 @@ router.get("/", async (req, res) => {
     const userInfo = await User.findOne({ username: token })
     res.render("profile/index", { user: userInfo })
     return
+  } else {
+    res.redirect("/")
+  }
+})
+
+// update user info
+router.post("/edit", async (req, res) => {
+  if (req.user) {
+    // check if req.user is present, authentication by middleware
+    const { image, name, bio, phone } = req.body
+
+    const imgResult = await cloudinary.uploader
+      .upload(image, {
+        folder: "group-chat"
+      })
+
+    let imageUrl = { url: imgResult.url, secure: imgResult.secure_url }
+
+    try {
+      const { token } = req.user
+      const user = await User.findOneAndUpdate({ username: token }, { name, bio, phone, image: imageUrl })
+
+      res.redirect("/profile")
+      return
+    } catch (error) {
+      res.render("error", { msg: "Error Updating profile" })
+    }
+
   } else {
     res.redirect("/")
   }
---
+++ static/js/handleEdit.js
@@ -0,0 +1,51 @@
+document.querySelector(".needs-validation").addEventListener("submit", (e) => {
+  // check edited information and send to database
+  e.preventDefault()
+  e.stopPropagation()
+  const form = e.currentTarget
+
+  if (!form.checkValidity()) {
+    form.classList.add("was-validated")
+  }
+  else {
+    const editedProfileInfo = {}
+
+    // image field 
+    const img = form.querySelector("input[type='file']").files[0]
+    const reader = new FileReader()
+    reader.readAsDataURL(img)
+    reader.addEventListener("loadend", function (e) {
+      const image = this.result
+      editedProfileInfo["image"] = image
+
+      // inputs field 
+      const inputs = form.querySelectorAll("input")
+      inputs?.forEach((input, ind) => {
+        if (ind > 0) {
+          editedProfileInfo[input.id] = input.value
+        }
+      })
+      // bio field 
+      const textarea = form.querySelector("textarea")
+      editedProfileInfo["bio"] = textarea.value
+
+      fetch("/profile/edit", {
+        method: "POST",
+        body: JSON.stringify(editedProfileInfo),
+        headers: {
+          "Content-Type": "application/json"
+        },
+      })
+        .then(() => {
+          document.querySelector(".profile").classList.remove("edit")
+          window.location.reload()
+        })
+        .catch(err => {
+          document.querySelector(".profile").classList.remove("edit")
+          window.location.reload()
+        })
+    })
+  }
+
+
+})
