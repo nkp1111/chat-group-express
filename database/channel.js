0bce86a0ac6a427ae1d63acc9406982ac78b08fd
nkp1111 <neerajp4321@gmail.com>
2023-05-13 17:06:49 +0530

    feat: add channel and message database

---
+++ database/channel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+
+const ChannelSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  description: String,
+  members: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+  ],
+  messages: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message"
+    }
+  ]
+}, { timestamps: true })
+
+const Channel = mongoose.model("Channel", ChannelSchema)
+
+module.exports = { Channel }
---
+++ database/message.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+
+const MessageSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+  },
+  writer: {
+    username: {
+      type: String,
+      required: true,
+    },
+    userImage: {
+      type: String,
+      required: true
+    }
+  },
+  date: {
+    type: Date,
+    default: new Date()
+  }
+})
+
+const Message = mongoose.model("Message", MessageSchema)
+
+module.exports = { Message }
